aaaa23f14699a710a7eb0fc3e8ac59e57b97df9f
Pau Riba <priba@cvc.uab.cat>
2018-10-14 23:08:54 +0200

    Add two adjacency matrices

--- src/models/layers.py
+++ src/models/layers.py
@@ -9,13 +9,13 @@ class GConv(nn.Module):
     Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
     """
 
-    def __init__(self, in_features, out_features, bias_bool=True, bn_bool=True):
+    def __init__(self, in_features, out_features, bias_bool=True, bn_bool=True, J=2):
         super(GConv, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
-
+        self.J = 2 # Number of W
         self.bias_bool = bias_bool
-        self.fc = nn.Linear(self.in_features, self.out_features, bias=self.bias_bool)
+        self.fc = nn.Linear(self.J*self.in_features, self.out_features, bias=self.bias_bool)
 
         self.bn_bool = bn_bool
         if self.bn_bool:
@@ -23,12 +23,16 @@ class GConv(nn.Module):
 
     
     def forward(self, x, W):
-        if W._nnz()==0:
-            output = torch.zeros(x.shape)
-            if x.is_cuda:
-                output = output.cuda()
-        else:
-            output = torch.spmm(W, x)
+        output = []
+        for w in W:
+            if w._nnz()==0:
+                out = torch.zeros(x.shape)
+                if x.is_cuda:
+                    out = out.cuda()
+                output.append(out)
+            else:
+                output.append(torch.spmm(w, x))
+        output = torch.cat(output, dim=1)
         output = self.fc(output)
 
         if self.bn_bool:
--- src/models/models.py
+++ src/models/models.py
@@ -1,21 +1,42 @@
+import torch
 import torch.nn as nn
 from .layers import GConv
 
 class GNN(nn.Module):
-    def __init__(self, in_feat, out_feat, dropout=True):
+    def __init__(self, in_feat, out_feat, nlayers=3, dropout=True):
         super(GNN, self).__init__()
-
-        self.gc1 = GConv(in_feat, 64)
-        self.gc2 = GConv(64, out_feat)
+        self.nlayers = nlayers
+        self.hid = 256
+        self.embedding = nn.Linear(in_feat, self.hid)
+        for i in range(1, nlayers):
+            module_gc = GConv(i*self.hid, self.hid)
+            self.add_module('gc{}'.format(i), module_gc)
+        self.gc_last = GConv(self.nlayers*self.hid, out_feat, bias_bool=False, bn_bool=False)
         self.dropout = nn.Dropout()
         self.nl = nn.LeakyReLU()
 
     def forward(self, g):
         x, W, g_size = g
-        x = self.nl(self.gc1(x, W))
+        Wid = self._wid(x.size(0))
+
+        x = self.nl(self.embedding(x))
+
+        for i in range(1, self.nlayers):
+            x_new = self.nl(self._modules['gc{}'.format(i)](x, [Wid, W]))
+            x = torch.cat([x, x_new], 1)
+
         x = self.dropout(x)
-        x = self.gc2(x, W)
+        x = self.gc_last(x, [Wid, W])
+
         return (x, W, g_size)
+
+    def _wid(self, s):
+        i = torch.LongTensor(range(s))
+        i = torch.stack((i,i))
+        Wid = torch.sparse.FloatTensor(i, torch.ones(s), torch.Size([s,s]))
+        if next(self.parameters()).is_cuda:
+            Wid = Wid.cuda()
+        return Wid
 
 
 if __name__=="__main__":
