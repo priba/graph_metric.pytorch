668692a9914c3b099932af80008847ed942fc50a
Pau Riba <priba@cvc.uab.cat>
2018-10-15 11:27:48 +0200

    Move evaluation to utils

--- src/test.py
+++ src/test.py
@@ -11,14 +11,11 @@ import glob
 import numpy as np
 import time
 import os
-from sklearn.metrics import average_precision_score
-from joblib import Parallel, delayed
-import multiprocessing
 
 # Own modules
 from options import Options
 from Logger import LogMetric
-from utils import load_checkpoint, graph_cuda, graph_to_sparse
+from utils import load_checkpoint, graph_cuda, graph_to_sparse, graph_cat, knn_accuracy, mean_average_precision
 from models import models
 
 __author__ = "Pau Riba"
@@ -74,62 +71,14 @@ def test(data_loader, gallery_loader, net, cuda, distance):
         target_query = torch.cat(target_query)
     
         # K-NN classifier
-        
-        predict = knn_predict(dist_matrix, target_gallery)
-        bacc = predict.eq(target_query).float().sum()
-        acc.update(100.0*bacc/predict.size(0))
+        acc.update(knn_accuracy(dist_matrix, target_gallery, target_query, k=5))
         
         # mAP retrieval
-        nq = target_query.size(0)
-        sim = 1./(1+dist_matrix)
-        str_sim = target_query.unsqueeze(1) == target_gallery
-        if sim.is_cuda:
-            sim, str_sim = sim.cpu(), str_sim.cpu()
-        sim, str_sim = sim.numpy(), str_sim.numpy()
-
-        num_cores = min(multiprocessing.cpu_count(), 32)
-        aps = Parallel(n_jobs=num_cores)(delayed(average_precision_score)(str_sim[iq], sim[iq]) for iq in range(nq))
-
-        meanap.update(np.mean(aps))
+        meanap.update(mean_average_precision(dist_matrix, target_gallery, target_query))
     batch_time.update(time.time()-start)
     print('* Test Acc {acc.avg:.3f}; mAP {meanap.avg: .3f}; Time x Test {b_time.avg:.3f}'
             .format(acc=acc, meanap=meanap, b_time=batch_time))
     return acc, meanap
-
-
-def knn_predict(dist_matrix, target_gallery, k=5):
-    ind = np.argsort(dist_matrix, axis=1)
-    sort_target = target_gallery[ind]
-    sort_target = sort_target[:,:k]
-    
-    # Counts
-    counts = np.zeros(sort_target.shape)
-    for i in range(k):
-        counts[:,i] = (sort_target[:, i].unsqueeze(1) == sort_target).long().sum(1)
-
-    predict_ind = counts.argmax(axis=1)
-    predict = [sort_target[i, pi] for i, pi in enumerate(predict_ind)]
-    return torch.stack(predict)
-
-def graph_cat(g):
-    nodes = []
-    indices = []
-    data = []
-    g_size = []
-    offset = 0
-    for gi in g:
-        nodes.append(gi[0])
-        indices.append(gi[1]._indices()+offset)
-        data.append(gi[1]._values())
-        g_size.append(gi[2])
-        offset = offset + gi[0].size(0)
-    g_out = (
-            torch.cat(nodes),
-            torch.cat(indices, dim=1),
-            torch.cat(data),
-            torch.cat(g_size)
-            )
-    return graph_to_sparse(g_out)
 
 
 def main():
--- src/utils.py
+++ src/utils.py
@@ -9,11 +9,15 @@ import torch
 import os
 import errno
 import numpy as np
+from sklearn.metrics import average_precision_score
+from joblib import Parallel, delayed
+import multiprocessing
 
 __author__ = 'Pau Riba'
 __email__ = 'priba@cvc.uab.cat'
 
 
+# Data
 def graph_to_sparse(g):
     g_out = ( g[0],
               torch.sparse.FloatTensor(g[1], g[2], (g[0].shape[0], g[0].shape[0])),
@@ -31,6 +35,67 @@ def graph_cuda(g):
     return g
 
 
+def graph_cat(g):
+    nodes = []
+    indices = []
+    data = []
+    g_size = []
+    offset = 0
+    for gi in g:
+        nodes.append(gi[0])
+        indices.append(gi[1]._indices()+offset)
+        data.append(gi[1]._values())
+        g_size.append(gi[2])
+        offset = offset + gi[0].size(0)
+    g_out = (
+            torch.cat(nodes),
+            torch.cat(indices, dim=1),
+            torch.cat(data),
+            torch.cat(g_size)
+            )
+    return graph_to_sparse(g_out)
+
+
+# Evaluation
+def knn_accuracy(dist_matrix, target_gallery, target_query, k=5):
+    # Predict
+    ind = np.argsort(dist_matrix, axis=1)
+    sort_target = target_gallery[ind]
+    sort_target = sort_target[:,:k]
+    
+    # Counts
+    counts = np.zeros(sort_target.shape)
+    for i in range(k):
+        counts[:,i] = (sort_target[:, i].unsqueeze(1) == sort_target).long().sum(1)
+
+    predict_ind = counts.argmax(axis=1)
+    predict = [sort_target[i, pi] for i, pi in enumerate(predict_ind)]
+    predict = torch.stack(predict)
+
+    # Accuracy
+    acc = predict.eq(target_query).float().sum()
+    acc = 100.0*acc/predict.size(0)
+    return acc
+
+
+def mean_average_precision(dist_matrix, target_gallery, target_query):
+    # Number of queries
+    nq = target_query.size(0)
+
+    # Distance to similarity
+    sim = 1./(1+dist_matrix)
+
+    # Relevant items
+    str_sim = target_query.unsqueeze(1) == target_gallery
+    if sim.is_cuda:
+        sim, str_sim = sim.cpu(), str_sim.cpu()
+    sim, str_sim = sim.numpy(), str_sim.numpy()
+
+    num_cores = min(multiprocessing.cpu_count(), 32)
+    aps = Parallel(n_jobs=num_cores)(delayed(average_precision_score)(str_sim[iq], sim[iq]) for iq in range(nq))
+    return np.mean(aps) 
+    
+# Checkpoints
 def save_checkpoint(state, directory, file_name):
 
     if not os.path.isdir(directory):
