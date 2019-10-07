5e6922479a196de654f6b52ce320dba3e16de0c6
Pau Riba <priba@cvc.uab.cat>
2019-10-07 13:52:42 +0200

    Add BatchNorm layer

--- src/models/models.py
+++ src/models/models.py
@@ -71,6 +71,9 @@ class GNN(nn.Module):
         self.layers = nn.ModuleList([
             GATConv(in_dim, hidden_dim, heads, residual=True, activation=F.relu),
             GATConv(heads*hidden_dim, hidden_dim, heads, feat_drop=0.3, residual=True, activation=F.relu)])
+        self.bn = nn.ModuleList([
+            nn.BatchNorm1d(heads*hidden_dim),
+            nn.BatchNorm1d(heads*hidden_dim)])
 
         self.last_layer = GATConv(heads*hidden_dim, out_dim, heads, residual=True)
 
@@ -78,9 +81,10 @@ class GNN(nn.Module):
         h = g.ndata['h']
 
 
-        for conv in self.layers:
+        for i, conv in enumerate(self.layers):
             h = conv(g, h)
             h = h.view(h.shape[0], -1)
+            h = self.bn[i](h)
 
         h = self.last_layer(g, h)
         h = h.mean(1)
